2d72465e8536dd9e8864de4a2688053977a39a35
unknown <you@example.com>
2019-11-22 10:58:51 +0100

    Improved character moving,added colision detection(need improvement)

--- js/index.js
+++ js/index.js
@@ -1,27 +1,163 @@
-let canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
 
-canvas.width = window.innerWidth*3/5;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth * 3 / 5;
+canvas.height = window.innerHeight * 4;
 
-let ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
-let sections = 8;
-let direction;
+const sections = 7;//Inside sections will be drawned all our bricks 
 let leftPressed = false;
 let rightPressed = false;
-let characterDimension = 30;
-let characterX = canvas.width/2 - characterDimension/2;
-let characterY = canvas.height - 30  - characterDimension;
-let moves = 0;
-let brickY= canvas.height-120;
+const size = 30;
+let brickNumber = 22;
+
+let character = {
+    x: canvas.width / 2 - size / 2,
+    y: canvas.height - 2 * size,
+}
+
+let moves = 0; //For auto jumping
+const difference = canvas.height / brickNumber;//Difference between each block
+let brickY = canvas.height - difference;
 let counter = 0;
-let brickPosition = [];
-let blockMargin = 20;
-let brickWidth = canvas.width/8-2*blockMargin;
+
+let brickMargin = 15;
+let brickWidth = canvas.width/sections - 2 * brickMargin;
 let brickHeight = 20;
 
-document.addEventListener("keydown" , keyDownHandler);
-document.addEventListener("keyup" , keyUpHandler);
+//Colision
+let colision = false;
+//If character jupm on block,start jumping from there-Need improvment!!
+function detectColision(){
+    for(let i=0;i<brickNumber;i++){
+        if(character.x > brickPosition[i][0] && character.x < brickPosition[i][0] + brickWidth 
+        && character.y +size> brickPosition[i][1] && character.y + size< brickPosition[i][1] + brickHeight){
+        colision = true; 
+        moves=0;
+        }
+    }
+}
+//If portal is detected-you passed level-Need improvment!
+function detectColisionPortal(){
+    for(let i=0;i<brickNumber;i++){
+        if(character.x > canvas.width/2-35 && character.x < canvas.width/2-35 + 70 
+            && character.y + size> 60 && character.y + size< 150){
+        console.log("You win");
+        }
+    }
+}
+
+
+
+//Draw sections for bricks
+function drawSection(){
+    for(let i = 0; i < sections ;i++){
+    ctx.beginPath();
+    ctx.rect(i*canvas.width/sections, 0, canvas.width/sections, canvas.height);
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+    ctx.closePath();
+    }
+}
+
+//Draw character
+function drawCharacter(){    
+    ctx.beginPath();
+    ctx.rect(character.x, character.y, size, size);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//Draw ground
+function drawGround(){
+    ctx.beginPath();
+    ctx.rect(0, canvas.height - size, canvas.width, size);
+    ctx.fillStyle = "darkgreen";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//Save brick position in brickPosition,so bricks will always be drawned in same place after clearRect
+let brickPosition = [];
+
+//Save bricks positions
+function brickPositionDeclaration(){
+    for(let i = 0; i < brickNumber; i++){
+    let random = Math.floor(Math.random() * sections);
+    let brickX = random * canvas.width / sections + brickMargin;
+    brickY -= 100;
+    brickPosition[i]=[brickX , brickY];
+    }
+}
+brickPositionDeclaration();
+//Draw bricks
+function drawBrick(){
+    for(let i = 0; i < brickNumber; i++){
+    ctx.beginPath();
+    ctx.rect(brickPosition[i][0], brickPosition[i][1], brickWidth, brickHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    }
+    //Draw portal for winning-Need improvment!!!!
+    ctx.beginPath();
+    ctx.rect(canvas.width/2 - 35 , 60 , 70, 150);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//Main function
+function draw(){
+    //Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //Call all functions inside draw function
+    drawSection();
+    drawCharacter();
+    drawGround();
+    drawBrick();
+    detectColision();
+    detectColisionPortal();
+
+    //This loop is moving character from top to bottom(auto-jumping)
+    for(let i = 0; i < 160;i++){
+        if(moves <= 80 ){
+            character.y -= 3;
+        }
+     
+        else if(moves <= 160 ){
+            character.y += 3;
+        }    
+        else{
+            moves = 0;
+        }
+        moves++;
+    }
+    //Move character right
+    if(rightPressed && character.x < canvas.width - size) {
+        character.x += 2.3;
+    }
+    //Move character left
+    else if(leftPressed && character.x > 0) {
+        character.x -= 2.3;
+    }
+    //If character go of the canvas from right,move him to the left
+    else if(rightPressed && character.x < canvas.width){
+        character.x = - size + 1;
+        character.x += 2.3;
+    }
+    //If character go of the canvas from left,move him to the right
+    else if(leftPressed && character.x < 0){
+        character.x = canvas.width - 1;
+        character.x += 2.3;
+    }
+    requestAnimationFrame(draw);
+}
+
+//Event Listeners
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
 
 function keyDownHandler(e){
     if(e.key === "Right" || e.key === "ArrowRight") {
@@ -39,85 +175,4 @@ function keyUpHandler(e){
         leftPressed = false;
     }
 }
-
-function drawSection(){
-    for(let i=0; i<sections ;i++){
-    ctx.beginPath();
-    ctx.rect(i*canvas.width/sections, 0, canvas.width/sections, canvas.height);
-    ctx.strokeStyle = "red";
-    ctx.stroke();
-    ctx.closePath();}
-}
-
-drawSection();
-
-function draw(){
-    let random = Math.floor(Math.random()*sections);
-    let brickX = random*canvas.width/8+blockMargin;
-    
-    ctx.beginPath();
-    ctx.rect(brickX, brickY, brickWidth, brickHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-    brickY -= 120;
-
-    brickPosition[counter] = [brickX , brickY];
-    requestAnimationFrame(draw);
-}
-
 draw();
-
-/*let colision = false;
-let fail = false;
-function colisionDetection(){
-    for(let i=0; i<sections ; i++){
-        if((characterX >= brickPosition[i][0] && characterX <=brickPosition[i][0]+brickWidth && characterY >= brickPosition[i][1] && characterY <= brickPosition[i][1] + brickHeight)
-        || (characterX + characterHeight >= brickPosition[i][0] && characterX + characterHeight<=brickPosition[i][0]+brickWidth && characterY-characterHeight >= brickPosition[i][1] && characterY-characterHeight <= brickPosition[i][1] + brickHeight)){
-            colision = true;
-        }
-        else{
-            colision=false;
-        }
-        console.log(colision);
-    }
-   
-} */
-
-function drawCharacter(){
-    ctx.clearRect(characterX, characterY, characterDimension, characterDimension);
-    ctx.beginPath();
-    ctx.rect(characterX, characterY, characterDimension, characterDimension);
-    ctx.strokeStyle = "#0095DD";
-    ctx.stroke();
-    ctx.closePath();
-    if(moves >= 0 && moves <= 20){
-        characterY -= 5;
-    }
-    else if(moves <= 40){
-        characterY += 5;
-    }
-    else{
-        moves = 0;
-    }
-
-    if(rightPressed && characterX < canvas.width - characterDimension) {
-        characterX += 10;
-    }
-    else if(leftPressed && characterX > 0) {
-        characterX -= 10;
-    }
-    moves++;
-    requestAnimationFrame(drawCharacter);
-}
-
-drawCharacter();
-
-function drawGround(){
-    ctx.beginPath();
-    ctx.rect(0, canvas.height-30, canvas.width, 30);
-    ctx.fillStyle = "darkgreen";
-    ctx.fill();
-    ctx.closePath();
-}
-drawGround();
